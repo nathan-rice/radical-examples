04a03f0e72c39cf55be6007094738c9009da2188
nathan <nathan.alexander.rice@gmail.com>
2016-04-12 09:16:38 -0400

    removed .idea

---
+++ todos/api.ts
@@ -0,0 +1,67 @@
+import * as Radical from '../../dist/radical.js';
+import * as Redux from 'redux';
+
+class TodoApi extends Radical.Namespace {
+    nextTodo = 0;
+    filters = {
+        all: todo => true,
+        active: todo => !todo.completed,
+        completed: todo => todo.completed
+    };
+    defaultState = {todos: [], todoFilter: this.filters.all};
+    
+    addTodo = Radical.Action.create({
+        initiator: function (action, text) {
+            return action.dispatch({todo: new TodoEntry(this.nextTodo++, text)});
+        },
+        reducer: function (state, action) {
+            state.todos = [...state.todos, action.todo];
+            return state;
+        }
+    });
+    
+    deleteTodo = Radical.Action.create({
+        initiator: function (action, todo) {
+            return action.dispatch({todo: todo});
+        },
+        reducer: function (state, action) {
+            state.todos = state.todos.filter(todo => action.todo.id != todo.id);
+            return state;
+        }
+    });
+    
+    toggleTodoCompleted = Radical.Action.create({
+        initiator: function (action, todo) {
+            return action.dispatch({todo: todo});
+        },
+        reducer: function (state, action) {
+            state.todos = state.todos.map(todo => {
+                if (todo.id == action.todo.id) return new TodoEntry(todo.id, todo.text, !todo.completed);
+                return todo;
+            });
+            return state;
+        }
+    });
+    
+    getTodos() {
+        let state = this.getState();
+        return state.todos.filter(state.todoFilter);
+    };
+    
+    setFilter = Radical.Action.create(function (action, filter) {
+        return action.dispatch({todoFilter: filter});
+    });
+    
+    getFilter() {
+        return this.getState().todoFilter;
+    }
+}
+
+export class TodoEntry {
+    constructor(public id: number, public text: string = "", public completed: boolean = false) {}
+}
+
+export var store = Redux.createStore(state => state);
+export var todo = TodoApi.create({getState: store.getState, dispatch: store.dispatch}) as TodoApi;
+
+store.replaceReducer(todo.reduce);
---
+++ todos/components.tsx
@@ -0,0 +1,124 @@
+/// <reference path="../../typings/main.d.ts" />
+
+import {todo, TodoEntry} from './api';
+import * as React from 'react';
+import * as ReactRedux from 'react-redux';
+
+export const App = () => (
+    <div>
+        <AddTodo />
+        <TodoList />
+        <Footer />
+    </div>
+);
+
+class AddTodo extends React.Component<any, any> {
+
+    handleSubmit(e) {
+        let trimmed, el = document.getElementById("todo-text") as HTMLInputElement;
+        e.preventDefault();
+        if (trimmed = el.value.trim()) {
+            todo.addTodo(trimmed);
+            el.value = '';
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input id="todo-text"/>
+                    <button type="submit">
+                        Add Todo
+                    </button>
+                </form>
+            </div>
+        )
+    }
+}
+
+const Footer = () => (
+    <p>
+        Show:
+        {" "}
+        <FilterLink filter={todo.filters.all}>
+            All
+        </FilterLink>
+        {", "}
+        <FilterLink filter={todo.filters.active}>
+            Active
+        </FilterLink>
+        {", "}
+        <FilterLink filter={todo.filters.completed}>
+            Completed
+        </FilterLink>
+    </p>
+);
+
+interface IFilterLinkProps {
+    active?: boolean;
+    children?: any;
+    filter: Function;
+}
+
+function filterLinkConnector(state, ownProps: IFilterLinkProps) {
+    return {active: ownProps.filter == todo.getFilter()};
+}
+
+@ReactRedux.connect(filterLinkConnector)
+class FilterLink extends React.Component<IFilterLinkProps, any> {
+    onClick(e) {
+        e.preventDefault();
+        todo.setFilter(this.props.filter);
+    }
+
+    render() {
+        if (this.props.active) {
+            return <span>{this.props.children}</span>
+        }
+
+        return (
+            <a href="#" onClick={this.onClick}>{this.props.children}</a>
+        )
+    }
+}
+
+interface ITodoProps {
+    onClick: Function;
+    todo: TodoEntry;
+}
+
+export class Todo extends React.Component<ITodoProps, any> {
+    getStyle = () => ({textDecoration: this.props.todo.completed ? 'line-through' : 'none'});
+
+    render() {
+        return (
+            <li onClick={this.props.onClick} style={this.getStyle()}>
+                {this.props.todo.text}
+            </li>
+        )
+    }
+}
+
+interface ITodoListProps {
+    todos?: TodoEntry[];
+    onTodoClick?: Function;
+}
+
+function todoListConnector() {
+    return {todos: todo.getTodos()}
+}
+
+@ReactRedux.connect(todoListConnector)
+class TodoList extends React.Component<ITodoListProps, any> {
+    render() {
+        return (
+            <ul>
+                {this.props.todos.map(todo =>
+                    <Todo key={todo.id} todo={todo} onClick={() => this.props.onTodoClick(todo.id)}/>
+                )}
+            </ul>
+        )
+    }
+}
+
